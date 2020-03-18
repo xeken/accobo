1378b9ef9e96913aa0596324795dd680fcf5f0df
ByungHyen <xeken@naver.com>
2020-03-18 21:26:40 +0900

    [UPDATE] add TextField

--- lib/pages/AddItemRoute.dart
+++ lib/pages/AddItemRoute.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/cupertino.dart';
 
-class AddItem extends StatelessWidget{
+class AddItem extends StatefulWidget{
+
+  @override
+  _AddItemState createState() => _AddItemState();
+}
+
+class _AddItemState extends State<AddItem> {
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+    _textController = TextEditingController(text: 'initial text');
+  }
+
   Widget build(BuildContext context){
     return CupertinoPageScaffold(
       child: Center(
-        child: Text("AddItem"),  
+        //child: Text("AddItem"),  
+        child: CupertinoTextField(controller: _textController),
       ),
       backgroundColor: CupertinoColors.white,
     );
   }
-
 }
